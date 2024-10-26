d06d809b17203214ab33d4640e7316936b960dc2
jbyiringiro <j.byiringir@alustudent.com>
2024-10-26 13:13:50 -0700

    Advanced api

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -11,14 +11,18 @@ def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts
     listed in a subreddit
     """
-    url ="https://reddit.com/r/{}.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
 
-     if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
-            print(post['data']['title'])
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
     else:
         print(None)
-
